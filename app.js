7f58dc307f11e711c6948b7dc8a1a3a67bedcb8b
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-10-25 16:15:56 +0500

    assignments

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("Allah Mohammad (S.A.W.W)");
+console.log(1+true);
+
+// let a = typeof (NaN);
+// console.log(a);
+
+// let a = typeof (undefined);
+// console.log(a);
+
+// let a = typeof ([""]);
+// console.log(a);
+
+// https://chatgpt.com/c/6718722d-73cc-8006-bec7-eed91210db74
+
+
+console.log(2*2*2);
